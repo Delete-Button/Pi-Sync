2208dd88f9e63fb9518e881c21669973c8a52e45
recarus <recarus47@gmail.com>
2019-05-22 13:13:43 -0700

    More Comments
    Added more descriptive comments to initscript, created new comments for remotescript, and made README look a little better.

--- initscript.sh
+++ initscript.sh
@@ -1,6 +1,6 @@
 #/bin/bash!
-#version 1, it has no error handling so don't 
-#type in anything wrong or you have to start over
+#ajaxx . reece . delete-button
+#version 1, it has no error handling so don't type in anything wrong or you have to start over
 #Set the Name of the SSH Keys
 echo 'Name the SSH Key'
 read sshKeyname
@@ -9,7 +9,7 @@ ssh-keygen -b 2048 -t rsa -f $sshKeyname
 #Set the IP address of the remote Pi
 echo 'Input the IP of the remote Pi'
 read IP
-#pushes the key and the remote script to the pi
+#Pushes the key and the remote script to the pi
 #Requires input from user for initial authentication
 scp $sshKeyname.pub remotescript.sh pi@$IP:~/
 #Also requires authentication from user, sets remotescript to executable, executes, and exits
@@ -31,11 +31,11 @@ echo '#/bin/bash!' >> Pi-Sync_$scriptName.sh
 echo "rsync --progress -avz --delete-before -e \"ssh -i /home/pi/.ssh/$sshKeyname\" /home/pi/Desktop/PiShare/$srcPath/ pi@$IP:/home/pi/Desktop/Share/" >> Pi-Sync_$scriptName.sh 
 chmod 700 Pi-Sync_$scriptName.sh
 ./Pi-Sync_$scriptName.sh
-#Sets it on the user's time table to be run every 5 minutes
+#Sets the new script on the user's time table to be run every 5 minutes
 crontab -l > tmpCron
 echo "0 5 * * * /home/pi/Documents/Pi-Sync/Pi-Sync_$scriptName.sh" > tmpCron
 crontab tmpCron 
-#clean up the garbage files
+#Clean up the garbage files
 rm tmpCron 
 rm $sshKeyname.pub
 echo 'Script Created, Syncing ready' 

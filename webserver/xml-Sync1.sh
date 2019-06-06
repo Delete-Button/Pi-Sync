73be4d8398c8b37e1b41f0acf4af37d6ab72cf8e
Mischevious Goat <recarus47@gmail.com>
2019-06-05 21:57:00 -0700

    not
    sure

--- webserver/xml-Sync1.sh
+++ webserver/xml-Sync1.sh
@@ -9,6 +9,7 @@
 if [ ! -d ~/.webbackup/ ]; then
   mkdir ~/.webbackup/;
 fi
+
 cp -r /var/www/test.com/ ~/.webbackup/
 
 # i hate using hardcoded passwords ,,, so here's some hardcoded passwords
@@ -16,22 +17,25 @@ cp -r /var/www/test.com/ ~/.webbackup/
 PASS=$()
 USR=$()
 DOM=$()
+
 # Now we mount
 sudo mount -t cifs '\\vista-akchin\VISTA\Extracts\ExtractResults\' /home/pi/.webbackup/mntpoint/ -o username=$USR,domain=$DOM,password=$PASS
 
 # Copy, mv will cripple the whole building; cp makes it happy
 cp /mnt/XML-Grab/NCRBOXOFCXML/boxofcxml.001 /var/www/
+
 # sudo, because linux trusts no one
-sudo umount /mnt/XML-Grab/
-# if we rm -rf the folder and its still mounted,,, goodbye job
+sudo umount /mnt/XML-Grab/4
+
+# We do NOT want this to be mounted for a long time, ALERT.txt will pop up 
 FILES=( /home/pi/.webbackup/XML-Grab/* )
 if [ ${#files[@]} -gt 0 ]; then 
   sudo umount /home/pi/.webbackup/XML-Grab/;
   touch /home/pi/Desktop/ALERT.txt
   echo 'XML-Grab Folder is Still mounted!' > /home/pi/Desktop/ALERT.txt
-  echo "un `umount /home/pi/.webbackup/XML-Grab/` and troubleshoot from there" >> /home/pi/Desktop/ALERT.txt
-
+  echo "use `umount /home/pi/.webbackup/XML-Grab/` and troubleshoot from there" >> /home/pi/Desktop/ALERT.txt
 fi
+
 # Move this new file into our working directory and replace its older versions.
 mv /var/www/boxofcxml.001 /var/www/test.com/boxofcxml.001
 mv /var/www/test.com/BOXFCXML.001 /var/www/test.com/BOXFCXML.xml

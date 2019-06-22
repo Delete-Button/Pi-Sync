a3183a8280e33d63f6dbcca28ab7169183c756e5
Mischevious Goat <recarus47@gmail.com>
2019-06-21 23:30:24 -0700

    lmao jk
    `~/`  is just bad

--- webserver/xml-Sync1.sh
+++ webserver/xml-Sync1.sh
@@ -6,17 +6,18 @@
 # content folder and cleans up after itself
 
 # Time to grab the running config, if anything goes wrong we have a back up!
-if [ ! -d ~/.webbackup/ ]; then
-  mkdir ~/.webbackup/;
-  mkdir ~/.webbackup/webdata/;
-  mkdir ~/.webbackup/mntpoint/;
+if [ ! -d home/pi/.webbackup/ ]; then
+  mkdir home/pi/.webbackup/;
+  mkdir home/pi/.webbackup/webdata/;
+  mkdir home/pi/.webbackup/mntpoint/;
 fi
+
 
 cp -r /usr/share/nginx/www/ ~/.webbackup/
 # Using the cred file instead of passing creds through the script
 sudo mount -t cifs '\\vista-akchin\VISTA\Extracts\ExtractResults\' /home/pi/.webbackup/mntpoint/ -o cred=/root/.cred/.cred
 # Copy, mv will cripple the whole building; cp makes it happy
-cp /home/pi/.webbackup/mntpoint/NCRBOXOFCXML/boxofcxml.001 /usr/share/nginx/www/
+cp /home/pi/.webbackup/mntpoint/NCRBOXOFCXML/boxofcxml.001 /usr/share/nginx/www/Theaterboards/
 
 # sudo, because linux trusts no one
 sudo umount /home/pi/.webbackup/mntpoint/
@@ -29,4 +30,4 @@ if [ ${#files[@]} -gt 0 ]; then
   echo 'XML-Grab Folder is Still mounted!' > /home/pi/Desktop/ALERT.txt
   echo "use `umount /home/pi/.webbackup/XML-Grab/` and troubleshoot from there" >> /home/pi/Desktop/ALERT.txt
 fi
-
+exit

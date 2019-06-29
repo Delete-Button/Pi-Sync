9de662a32fdf15f0d2301d4e3b79040ca0b7deee
Mischevious Goat <recarus47@gmail.com>
2019-06-28 22:31:26 -0700

    Added the webdata-Sync Script
    please don't hak me

---
+++ webserver/webdata-Sync.sh
@@ -0,0 +1,9 @@
+#/bin/bash!
+# ajaxx . delete-button
+mount -t cifs '192.168.240.239\web\' /home/pi/testfolder -o cred=/home/pi/.ff
+cp -r /home/pi/testfolder/* /usr/share/nginx/www/
+umount /home/pi/testfolder/ 
+service nginx restart 
+service php7.2-fpm restart
+echo 'Done, Thanks Bubbs'
+exit

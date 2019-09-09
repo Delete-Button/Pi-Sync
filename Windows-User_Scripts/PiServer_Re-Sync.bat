c47c6aee9fb046b125cc75f1a50dc5658a677963
Mischevious Goat <recarus47@gmail.com>
2019-09-08 21:42:42 -0700

    Lmao
    The file has been renamed on the server without the .sh updating on this

--- Windows-User_Scripts/PiServer_Re-Sync.bat
+++ Windows-User_Scripts/PiServer_Re-Sync.bat
@@ -3,5 +3,5 @@
 :: Runs the `webserver/xml-Sync.sh` script
 echo Syncing Pi Server with Vista...
 echo Type in Password for both Prompts
-ssh -t pi@192.168.240.239 "cd ~/Pi-Sync/webserver/; sudo ./xml-Sync.sh; exit;"
+ssh -t pi@192.168.240.239 "cd ~/Pi-Sync/webserver/; sudo ./xml-Sync; exit;"
 echo Done 
